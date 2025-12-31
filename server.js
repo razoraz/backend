94d16125e1734e8eae151f49067adaf5f6b01dc8
Veri Ramadhan <93845135+razoraz@users.noreply.github.com>
2025-12-31 21:54:25 +0700

    Update server.js

--- server.js
+++ server.js
@@ -11,6 +11,7 @@ const PORT = process.env.PORT || 5000;
 // Middleware
 app.use(cors());
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use("/uploads", express.static("uploads"));
 
 // Tes endpoint
