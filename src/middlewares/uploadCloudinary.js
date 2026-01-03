1369e87f9b1eefac2f9a53256d4584fda4ef8e18
Veri Ramadhan <93845135+razoraz@users.noreply.github.com>
2026-01-04 06:03:21 +0700

    Update uploadCloudinary.js

--- src/middlewares/uploadCloudinary.js
+++ src/middlewares/uploadCloudinary.js
@@ -1,25 +1,24 @@
-import multer from 'multer';
-import { CloudinaryStorage } from 'multer-storage-cloudinary';
-import cloudinary from '../config/cloudinary.js';
+import multer from "multer";
+import { CloudinaryStorage } from "multer-storage-cloudinary";
+import cloudinary from "../config/cloudinary.js";
 
 const storage = new CloudinaryStorage({
   cloudinary,
   params: (req, file) => {
     // Tentukan folder berdasarkan endpoint
-    let folder = 'others';
+    let folder = "others";
 
-    if (req.originalUrl.includes('/menu')) {
-      folder = 'menu';
-    } else if (req.originalUrl.includes('/feedback')) {
-      folder = 'feedback';
-    } else if (req.originalUrl.includes('/event')) {
-      folder = 'event';
+    if (req.originalUrl.includes("/menu")) {
+      folder = "menu";
+    } else if (req.originalUrl.includes("/feedback")) {
+      folder = "feedback";
+    } else if (req.originalUrl.includes("/event")) {
+      folder = "event";
     }
 
     return {
       folder,
-      resource_type: 'auto', // ⭐ PENTING
-      allowed_formats: ['jpg', 'jpeg', 'png', 'mp4', 'webm', 'mov'],
+      allowed_formats: ["jpg", "jpeg", "png"],
       public_id: `${folder}-${Date.now()}`,
     };
   },
