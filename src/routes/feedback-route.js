3709168284faad6a158c377529ead1a2f2d38dc8
Veri Ramadhan <93845135+razoraz@users.noreply.github.com>
2026-01-03 16:44:58 +0700

    Update feedback-route.js

--- src/routes/feedback-route.js
+++ src/routes/feedback-route.js
@@ -1,11 +1,15 @@
-import express from 'express';
-import upload from '../middlewares/uploadCloudinarys.js';
-import { getFeedbacks, createFeedback, deleteFeedbackById } from '../controllers/feedback.js';
+import express from "express";
+import upload from "../middlewares/uploadCloudinary.js";
+import {
+  getFeedbacks,
+  createFeedback,
+  deleteFeedbackById,
+} from "../controllers/feedback.js";
 
 const router = express.Router();
 
-router.get('/', getFeedbacks);
-router.post('/tambah-feedback', upload.single('image'), createFeedback);
-router.delete('/:id_feedback', deleteFeedbackById);
+router.get("/", getFeedbacks);
+router.post("/tambah-feedback", upload.single("image"), createFeedback);
+router.delete("/:id_feedback", deleteFeedbackById);
 
 export default router;
