f24b83895891c548af5b26a7ef51d74e7e507584
Veri Ramadhan <93845135+razoraz@users.noreply.github.com>
2026-01-03 16:32:52 +0700

    Update feedback-route.js

--- src/routes/feedback-route.js
+++ src/routes/feedback-route.js
@@ -1,30 +1,15 @@
 import express from "express";
-import multer from "multer";
-import path from "path";
-import { getFeedbacks, createFeedback , deleteFeedbackById } from "../controllers/feedback.js";
+import upload from "../middlewares/uploadCloudinary.js";
+import {
+  getFeedbacks,
+  createFeedback,
+  deleteFeedbackById,
+} from "../controllers/feedback.js";
 
 const router = express.Router();
 
-// Folder upload harus ada
-const storage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    cb(null, "uploads/feedback"); // pastikan folder ada
-  },
-  filename: (req, file, cb) => {
-    cb(
-      null,
-      Date.now() + "-" + Math.round(Math.random() * 1e9) + path.extname(file.originalname)
-    );
-  },
-});
-
-const upload = multer({ storage: storage });
-
-// GET semua feedback
 router.get("/", getFeedbacks);
-// POST tambah feedback
 router.post("/tambah-feedback", upload.single("image"), createFeedback);
-// DELETE feedback by ID
 router.delete("/:id_feedback", deleteFeedbackById);
 
 export default router;
