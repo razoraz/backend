4540dc98b9c040023fbd8d00ad55157fbce42e4e
Veri Ramadhan <93845135+razoraz@users.noreply.github.com>
2026-01-10 16:45:15 +0700

    Create kategori-route.js

---
+++ src/routes/kategori-route.js
@@ -0,0 +1,15 @@
+import express from "express";
+import {
+  getKategori,
+  tambahKategori,
+  hapusKategori,
+  perbaruiKategori,
+} from "../controllers/kelola-kategori.js";
+
+const router = express.Router();
+
+// GET semua kategori
+router.get("/", getKategori);
+router.post("/", tambahKategori);
+router.delete("/:id_kategori", hapusKategori);
+router.put("/:id_kategori", perbaruiKategori);
