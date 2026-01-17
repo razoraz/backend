f630ddb07f34ee0934f771952b049b7f107286e8
Veri Ramadhan <93845135+razoraz@users.noreply.github.com>
2026-01-17 12:34:31 +0700

    Update midtrans.js

--- src/config/midtrans.js
+++ src/config/midtrans.js
@@ -1,7 +1,7 @@
 import midtransClient from 'midtrans-client';
 
 export const snap = new midtransClient.Snap({
-    isProduction: true,
+    isProduction: false,
     serverKey: process.env.MIDTRANS_SERVER_KEY,
     clientKey: process.env.MIDTRANS_CLIENT_KEY,
 });
